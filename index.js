a52b263886ddcb7ab25debd5cf7fbcf8d064d238
Javier Carrillo <stringparser@gmail.com>
2014-07-08 00:14:01 +0200

    Changed push behaviour to only accept functions

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-exports = module.exports = require('.lib/que.js')
+exports = module.exports = require('./lib/que.js');
+
+var Queue = module.exports;
+var que = new Queue();
+console.log('%s', que);
