152795f5d7a7081c7e32893a744c409925c8b45d
Javier Carrillo <stringparser@gmail.com>
2014-07-08 21:18:25 +0200

    Changed and added some events to the forEach method

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-var Queue = require('./lib/que.js');
-var que = new Queue();
+var Queue = require('./lib/Queue.js');
+// > undefined
+
+//
+// > undefined
+// Testing
+// > undefined
+//
+// > undefined
+var  que = new Queue(Math.random, Math.sin, Math.cos);
+// > undefined
+var methods = Object.getOwnPropertyNames;
+// > undefined
+
+que.forEach(function(el){
+  console.log('now run %s', el)
+})
+// > starts with function random() { [native code] }
+// > function random() { [native code] }
+// > There is no after observer
+// > function sin() { [native code] }
+// > There is no after observer
+// > function cos() { [native code] }
+// > function cos() { [native code] } finished
+// > [ [Function: random],
+// >   [Function: sin],
+// >   [Function: cos] ]
+
 
 exports = module.exports = que;
+// > [ [Function: random],
+// >   [Function: sin],
+// >   [Function: cos] ]
