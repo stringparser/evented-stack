78d3f3eef742e22dce6fc3b5e1883c6470f3df06
Javier Carrillo <stringparser@gmail.com>
2014-07-08 22:09:10 +0200

    moving on and emit methods to proto.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var Queue = require('./lib/Queue.js');
+var Queue = require('./lib/Queue');
+    methods = require('./lib/methods');
+
+/*
+ * Add methods to the prototype
+ */
+
+Queue = methods(Queue);
 // > undefined
 
 //
@@ -8,11 +15,25 @@ var Queue = require('./lib/Queue.js');
 //
 // > undefined
 var  que = new Queue(Math.random, Math.sin, Math.cos);
+// > undefined
 
 
 que.forEach(function(el){
   console.log('now run %s', el)
 })
+// > start event before function random() { [native code] }
+// > now run function random() { [native code] }
+// > next event function random() { [native code] }
+// > now run function sin() { [native code] }
+// > next event function sin() { [native code] }
+// > now run function cos() { [native code] }
+// > ending event after function cos() { [native code] }
+// > [ [Function: random],
+// >   [Function: sin],
+// >   [Function: cos] ]
 
 
 exports = module.exports = que;
+// > [ [Function: random],
+// >   [Function: sin],
+// >   [Function: cos] ]
