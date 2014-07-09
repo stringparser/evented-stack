d92079bb8592efadff5f08b38ce694c831e08191
Javier Carrillo <stringparser@gmail.com>
2014-07-09 18:04:33 +0200

    sanity checks

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var Stack = require('./lib/Queue');
-    methods = require('./lib/methods');
+var obsArray = require('./lib/EventedArray');
+// > undefined
+    obs = obsArray(1,2,3);
+// > [ 1, 2, 3 ]
 
-/*
- * Add methods to the prototype
- */
-
-Queue = methods(Queue);
-
-//
-// Testing
-//
-var  que = new Queue(Math.random, Math.sin, Math.cos);
-
-
-que.forEach(function(el){
-  console.log('now run %s', el)
-})
-
+console.log('%s', obs.forEach)
+// > function forEach() { [native code] }
+// > undefined
 
 exports = module.exports = que;
+// > ReferenceError: que is not defined
+// >     at repl:1:28
+// >     at REPLServer.self.eval (repl.js:112:21)
+// >     at repl.js:249:20
+// >     at REPLServer.self.eval (repl.js:122:7)
+// >     at Interface.<anonymous> (repl.js:239:12)
+// >     at Interface.emit (events.js:95:17)
+// >     at Interface._onLine (readline.js:202:10)
+// >     at Interface._line (readline.js:531:8)
+// >     at Interface._ttyWrite (readline.js:767:16)
+// >     at ReadStream.onkeypress (readline.js:99:10)
