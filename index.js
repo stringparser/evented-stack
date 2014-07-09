c0727b366dba9cf2fbbe184bb7405453b0ebd8d4
Javier Carrillo <stringparser@gmail.com>
2014-07-10 00:00:35 +0200

    forEach works. Added Silent property to manage unsubscribing to events

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-var obsArray = require('./lib/EventedArray');
-// > undefined
-    obs = obsArray(1,2,3);
-// > [ 1, 2, 3 ]
+var stack = new require('./lib/EventedArray')(1,2,3)
 
-console.log('%s', obs.forEach)
-// > function forEach() { [native code] }
-// > undefined
+stack.forEach(function(el){
+  console.log(el)
+})
 
-exports = module.exports = que;
-// > ReferenceError: que is not defined
-// >     at repl:1:28
-// >     at REPLServer.self.eval (repl.js:112:21)
-// >     at repl.js:249:20
-// >     at REPLServer.self.eval (repl.js:122:7)
-// >     at Interface.<anonymous> (repl.js:239:12)
-// >     at Interface.emit (events.js:95:17)
-// >     at Interface._onLine (readline.js:202:10)
-// >     at Interface._line (readline.js:531:8)
-// >     at Interface._ttyWrite (readline.js:767:16)
-// >     at ReadStream.onkeypress (readline.js:99:10)
+exports = module.exports = stack;
