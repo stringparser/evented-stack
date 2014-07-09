122ce29bd8072d940798967c1dd46f6103f0da80
Javier Carrillo <stringparser@gmail.com>
2014-07-09 15:46:58 +0200

    forgot to clean dirty tests

--- index.js
+++ index.js
@@ -6,34 +6,16 @@ var Queue = require('./lib/Queue');
  */
 
 Queue = methods(Queue);
-// > undefined
 
 //
-// > undefined
 // Testing
-// > undefined
 //
-// > undefined
 var  que = new Queue(Math.random, Math.sin, Math.cos);
-// > undefined
 
 
 que.forEach(function(el){
   console.log('now run %s', el)
 })
-// > start event before function random() { [native code] }
-// > now run function random() { [native code] }
-// > next event function random() { [native code] }
-// > now run function sin() { [native code] }
-// > next event function sin() { [native code] }
-// > now run function cos() { [native code] }
-// > ending event after function cos() { [native code] }
-// > [ [Function: random],
-// >   [Function: sin],
-// >   [Function: cos] ]
 
 
 exports = module.exports = que;
-// > [ [Function: random],
-// >   [Function: sin],
-// >   [Function: cos] ]
